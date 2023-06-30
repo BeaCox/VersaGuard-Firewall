aa229a9e020e527c09b38f9065f0915fd9c22d3e
BeaCox <root@beacox.space>
2023-06-30 13:36:46 +0800

    fix delete_func bug: wrong order when deleting

--- src/core_func.c
+++ src/core_func.c
@@ -78,7 +78,17 @@ void on_import_button_clicked(GtkButton *button, gpointer data)
     gtk_widget_destroy(dialog);
 }
 
-
+// 如果ip输入框旁边的secondary icon被点击，弹窗提示用户输入正确的ip地址
+void on_ip_entry_icon_press(GtkEntry *entry, GtkEntryIconPosition icon_pos, GdkEvent *event, gpointer data)
+{
+    GtkWidget *hint_dialog = gtk_message_dialog_new(NULL,
+                                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                    GTK_MESSAGE_ERROR,
+                                                    GTK_BUTTONS_CLOSE,
+                                                    "Please enter a valid IP address!");
+    gtk_dialog_run(GTK_DIALOG(hint_dialog));
+    gtk_widget_destroy(hint_dialog);
+}
 // 添加按钮回调函数
 void on_add_button_clicked(GtkButton *button, gpointer data)
 {
@@ -184,7 +194,6 @@ void on_export_button_clicked(GtkButton *button, gpointer data)
 }
 
 
-// 删除按钮回调函数
 void on_delete_button_clicked(GtkButton *button, gpointer data)
 {
     // 如果没有选中任何行，给出相应的提示
@@ -199,6 +208,7 @@ void on_delete_button_clicked(GtkButton *button, gpointer data)
         gtk_widget_destroy(hint_dialog);
         return;
     }
+
     // 创建确认对话框
     GtkWidget *confirm_dialog = gtk_message_dialog_new(NULL,
                                                        GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -219,39 +229,55 @@ void on_delete_button_clicked(GtkButton *button, gpointer data)
         // 获取选中的行
         GList *selectedRows = gtk_tree_selection_get_selected_rows(selection, NULL);
         GList *row = selectedRows;
-        // 遍历选中的行
+
+        // 存储要删除的行索引
+        GArray *rowsToDelete = g_array_new(FALSE, FALSE, sizeof(gint));
+
+        // 遍历选中的行，记录要删除的行索引
         while (row)
         {
             GtkTreePath *path = (GtkTreePath *)(row->data);
-            GtkTreeIter iter;
-            gtk_tree_model_get_iter(GTK_TREE_MODEL(liststore), &iter, path);
-            // 获取选中行的id
-            gint id;
-            gtk_tree_model_get(GTK_TREE_MODEL(liststore), &iter, 0, &id, -1);
-            // 从数据库中删除选中行
-            if(!deleteData(id)){
-                // 提示用户删除失败
-                GtkWidget *error_dialog = gtk_message_dialog_new(NULL,
-                                                                 GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                                 GTK_MESSAGE_ERROR,
-                                                                 GTK_BUTTONS_CLOSE,
-                                                                 "Delete failed! Please check permissions!");
-                gtk_dialog_run(GTK_DIALOG(error_dialog));
-                gtk_widget_destroy(error_dialog);
-
-                // 释放内存
-                g_list_free(selectedRows);
-                gtk_widget_destroy(confirm_dialog);
-                return;
-            }
-            // 从liststore中删除选中行
-            gtk_list_store_remove(liststore, &iter);
-            // 释放内存
+            g_array_append_val(rowsToDelete, gtk_tree_path_get_indices(path)[0]);
             row = g_list_next(row);
-            gtk_tree_path_free(path);
         }
+
+        // 逆序遍历要删除的行索引，以防删除后影响后续行的位置
+        for (gint i = rowsToDelete->len - 1; i >= 0; i--)
+        {
+            gint index = g_array_index(rowsToDelete, gint, i);
+            GtkTreeIter iter;
+            if (gtk_tree_model_iter_nth_child(GTK_TREE_MODEL(liststore), &iter, NULL, index))
+            {
+                // 获取选中行的id
+                gint id;
+                gtk_tree_model_get(GTK_TREE_MODEL(liststore), &iter, 0, &id, -1);
+                // 删除选中行
+                gtk_list_store_remove(liststore, &iter);
+                // 删除数据库中的记录
+                if (!deleteData(id))
+                {
+                    gtk_widget_destroy(confirm_dialog);
+                    // 提示用户删除数据库失败
+                    GtkWidget *error_dialog = gtk_message_dialog_new(NULL,
+                                                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                                    GTK_MESSAGE_ERROR,
+                                                                    GTK_BUTTONS_CLOSE,
+                                                                    "Delete database failed! Please check permissions!");
+                    gtk_dialog_run(GTK_DIALOG(error_dialog));
+                    gtk_widget_destroy(error_dialog);
+                    // 释放内存
+                    g_list_free(selectedRows);
+                    g_array_free(rowsToDelete, TRUE);
+                    return;
+                }
+            }
+        }
+        g_array_free(rowsToDelete, TRUE);
+
         // 更新数据库
-        if(!showData(liststore)){
+        if (!showData(liststore))
+        {
+            gtk_widget_destroy(confirm_dialog);
             // 提示用户更新数据库失败
             GtkWidget *error_dialog = gtk_message_dialog_new(NULL,
                                                              GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -262,9 +288,9 @@ void on_delete_button_clicked(GtkButton *button, gpointer data)
             gtk_widget_destroy(error_dialog);
             // 释放内存
             g_list_free(selectedRows);
-            gtk_widget_destroy(confirm_dialog);
             return;
         }
+
         // 提示用户删除成功，并显示删除记录数量
         // 关闭对话框
         gtk_widget_destroy(confirm_dialog);
@@ -275,14 +301,17 @@ void on_delete_button_clicked(GtkButton *button, gpointer data)
                                                            "Delete successful! %d records deleted!",
                                                            g_list_length(selectedRows));
         gtk_dialog_run(GTK_DIALOG(success_dialog));
+        // 如果点击了关闭按钮，销毁对话框
         gtk_widget_destroy(success_dialog);
         // 释放内存
         g_list_free(selectedRows);
         return;
     }
+
     // 如果用户选择了否，关闭对话框
     gtk_widget_destroy(confirm_dialog);
 }
+
 
 
 // 全选按钮回调函数
