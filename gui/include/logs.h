5775e27ba2df58a5f90dbd41f1d7f22640e5e7ee
BeaCox <root@beacox.space>
2023-07-16 00:08:44 +0800

    add log search_func

--- gui/include/logs.h
+++ gui/include/logs.h
@@ -6,12 +6,19 @@
 #include <sys/stat.h>
 #include "global.h"
 
-
+// 用来显示LOG文件新一行的字符串最大大小
 #define LOG_BUF_SIZE 1024
 
+// LOG文件发生变化的回调函数
 void file_changed_callback(GFileMonitor *monitor, GFile *file, GFile *other_file, GFileMonitorEvent event_type, gpointer user_data);
+// 建立LOG文件的软连接
 void createLogLink();
+// LOG搜索框
+void on_log_searchentry_search_changed(GtkSearchEntry *search_entry, gpointer user_data);
+// 切换搜索结果
+gboolean on_log_searchentry_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
 
 #endif // LOGS_H
+
 
 
--- gui/src/logs.c
+++ gui/src/logs.c
@@ -56,8 +56,7 @@ void file_changed_callback(GFileMonitor *monitor, GFile *file, GFile *other_file
             char time_str[24];
             memset(time_str, 0, 24);
             sprintf(time_str, "%s %s", date, time);
-            // 应用time_tag（补充完整前面的[）
-            gtk_text_buffer_insert_with_tags_by_name(buffer, &iter, "[", -1, "time", NULL);
+            // 应用time_tag
             gtk_text_buffer_insert_with_tags_by_name(buffer, &iter, time_str, -1, "time", NULL);
             // 插入空格
             gtk_text_buffer_insert(buffer, &iter, " ", -1);
@@ -119,4 +118,79 @@ void createLogLink()
         symlink(log_file, app_log_file);
     }
 }
+
+// 全局变量，用于保存搜索结果的位置信息
+GtkTextIter search_start, search_end;
+
+// 回调函数：搜索文本发生变化时的处理
+void on_log_searchentry_search_changed(GtkSearchEntry *search_entry, gpointer user_data)
+{
+    GtkTextView *text_view = GTK_TEXT_VIEW(user_data);
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
+    const gchar *search_text = gtk_entry_get_text(GTK_ENTRY(search_entry));
+
+    GtkTextIter start, end;
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_get_end_iter(buffer, &end);
+
+    if (search_text[0] != '\0')
+    {
+        gboolean found = gtk_text_iter_forward_search(&start, search_text, GTK_TEXT_SEARCH_VISIBLE_ONLY, &start, &end, NULL);
+
+        if (found)
+        {
+            // 保存搜索结果的位置信息
+            search_start = start;
+            search_end = end;
+
+            gtk_text_buffer_select_range(buffer, &start, &end);
+            gtk_text_view_scroll_to_iter(text_view, &start, 0.0, TRUE, 0.5, 0.5);
+        }
+        else
+        {
+            gtk_text_buffer_select_range(buffer, &start, &start);
+        }
+    }
+    else
+    {
+        gtk_text_buffer_select_range(buffer, &start, &start);
+    }
+}
+
+// 回调函数：处理按键事件
+gboolean on_log_searchentry_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
+{
+    if (event->type == GDK_KEY_PRESS)
+    {
+        GtkSearchEntry *search_entry = GTK_SEARCH_ENTRY(widget);
+        GtkTextView *text_view = GTK_TEXT_VIEW(user_data);
+        GtkTextBuffer *buffer = gtk_text_view_get_buffer(text_view);
+
+        if (event->keyval == GDK_KEY_Up || event->keyval == GDK_KEY_KP_Up || event->keyval == GDK_KEY_Down || event->keyval == GDK_KEY_KP_Down)
+        {
+            gboolean found = FALSE;
+
+            // 根据按键判断是切换上一个匹配还是下一个匹配
+            if (event->keyval == GDK_KEY_Up || event->keyval == GDK_KEY_KP_Up)
+            {
+                found = gtk_text_iter_backward_search(&search_start, gtk_entry_get_text(GTK_ENTRY(search_entry)), GTK_TEXT_SEARCH_VISIBLE_ONLY, &search_start, &search_end, NULL);
+            }
+            else if (event->keyval == GDK_KEY_Down || event->keyval == GDK_KEY_KP_Down)
+            {
+                found = gtk_text_iter_forward_search(&search_end, gtk_entry_get_text(GTK_ENTRY(search_entry)), GTK_TEXT_SEARCH_VISIBLE_ONLY, &search_start, &search_end, NULL);
+            }
+
+            if (found)
+            {
+                gtk_text_buffer_select_range(buffer, &search_start, &search_end);
+                gtk_text_view_scroll_to_iter(text_view, &search_start, 0.0, TRUE, 0.5, 0.5);
+            }
+        }
+    }
+
+    // 继续传递事件
+    return FALSE;
+}
+
+
 
